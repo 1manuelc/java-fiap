7db3c052446ae7582a71c885dd76c4d87a670352
Manuel Carlos <manuelwn21@gmail.com>
2023-02-20 20:56:48 -0300

    Update 05-strings

---
+++ Chapter05/04-arrays/ex-Arrays/src/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    private String name;
+    private String description;
+    private float value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name= " + name + ", description= " + description + ", value= " + value + "]";
+    }
+}
